05a616f5a23b75896ea80bb4860fdc2e3d6ca02e
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-09 17:55:17 +0300

    am adaugat si partea de css

--- scriptSprint11.js
+++ scriptSprint11.js
@@ -2,7 +2,6 @@ const textAdvice = document.getElementById("advice-text");
 
 async function getAdvice() {
     const url = "https://api.adviceslip.com/advice";
-
     try {
         const response = await fetch(url);
         const data = await response.json();
@@ -16,7 +15,8 @@ async function getAdvice() {
 }
 
 function renderAdvice(id, advice) {
-    textAdvice.innerHTML = `Advice #${id} <br> ${advice}`;
+    textAdvice.innerHTML = `<span id="advice-id"> Advice #${id} </span><br> ${advice}`;
+    textAdvice.classList.add("active")
 }
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("dice-button");
