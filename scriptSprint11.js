c153eafb9e84a58c44e2e0f1b4f4f0a0c7610bd6
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-09 18:23:32 +0300

    am terminat tema

--- scriptSprint11.js
+++ scriptSprint11.js
@@ -17,9 +17,26 @@ async function getAdvice() {
 function renderAdvice(id, advice) {
     textAdvice.innerHTML = `<span id="advice-id"> Advice #${id} </span><br> ${advice}`;
     textAdvice.classList.add("active")
+    tellMeAnAdvice(advice)
+
 }
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("dice-button");
     button.addEventListener('click', getAdvice);
 });
+
+function tellMeAnAdvice(advise) {
+    VoiceRSS.speech({
+        key: "52bfbd75e1c24955831329926a53c5ed",
+        src: advise,
+        hl: "en-us",
+        v: "Nancy",
+        r: 0,
+        c: "mp3",
+        f: "44khz_16bit_stereo",
+        ssml: false,
+    });
+};
+
+
 
